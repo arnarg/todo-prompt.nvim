ce67dff1b484b38b595261e2e040b989064bc9de
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-02-10 20:33:23 +0100

    Adding extmark to display the parsed date

--- lua/todo-prompt.lua
+++ lua/todo-prompt.lua
@@ -20,12 +20,14 @@ local popup_options = {
 local M = {}
 
 M.AddTask = function(cb)
-	local ns = vim.api.nvim_create_namespace("ToDoPrompt")
+	local ns = vim.api.nvim_create_namespace("todo_prompt")
+	local mark_id = 0
 	local last_start = 0
 	local last_stop = 0
 	local prompt = "> "
 
-	vim.api.nvim_command("hi ToDoPromptDate cterm=underline gui=underline")
+	vim.api.nvim_command("hi todo_prompt_date cterm=underline gui=underline")
+	vim.api.nvim_command("hi todo_prompt_extmark ctermfg=gray guifg=gray")
 
 	local input = Input(popup_options, {
 		prompt = prompt,
@@ -36,24 +38,44 @@ M.AddTask = function(cb)
 			end
 		end,
 		on_change = function(val, b)
-			local _, _, start, stop = parser.parse_task(val)
+			local d = os.time()
+			local _, date, start, stop = parser.parse_task(val, d)
 			start = start + #prompt
 			stop = stop + #prompt
+			-- matched indexes have changed
 			if start ~= last_start or stop ~= last_stop then
 				vim.api.nvim_buf_clear_namespace(b, ns, 0, -1)
 				if start ~= stop then
 					-- nvim must expect some different indexing because start is always
 					-- off by one
-					vim.api.nvim_buf_add_highlight(b, ns, "ToDoPromptDate", 0, start-1, stop)
+					vim.api.nvim_buf_add_highlight(b, ns, "todo_prompt_date", 0, start-1, stop)
 				end
 				last_start = start
 				last_stop = stop
+			end
+			-- parsed date is not the same as we passed to parse_task
+			if date ~= d then
+				opts = {
+					virt_text = {{os.date("%d/%m/%y %H:%M", date), "todo_prompt_extmark"}},
+					virt_text_pos = "right_align",
+					hl_mode = "blend",
+				}
+				if mark_id ~= 0 then
+					opts.id = mark_id
+				end
+				_id = vim.api.nvim_buf_set_extmark(b, ns, 0, 0, opts)
+				if mark_id == 0 then
+					mark_id = _id
+				end
+			elseif date == d and mark_id ~= 0 then
+				vim.api.nvim_buf_del_extmark(b, ns, mark_id)
+				mark_id = 0
 			end
 		end,
 	})
 	input:mount()
 
-	input:map("i", "<Esc>", input.input_props.on_close, { noremap = true })
+	input:map("n", "<Esc>", input.input_props.on_close, { noremap = true })
 end
 
 return M
